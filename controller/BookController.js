b38b04f41185b95ce46bd756ad85beecec9094f0
Galaxy-104 <js_park95@naver.com>
2024-10-07 16:53:42 +0900

    feat : Implement inquiry into book by id

--- controller/BookController.js
+++ controller/BookController.js
@@ -4,7 +4,8 @@ const { StatusCodes } = require('http-status-codes');
 const allBooks = (req, res) => {
     // 전체 도서 리스트
     let sql = `SELECT * FROM books`;
-    conn.query(sql, (err, results) => {
+    conn.query(sql, 
+        (err, results) => {
         if (err) {
             console.log(err);
             return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
@@ -15,7 +16,23 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    res.json({ message : "개별 도서 조회"})
+    const { id } = req.params;
+
+    let sql = `SELECT * FROM books WHERE id = ?`;
+    conn.query(sql, id, 
+        (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+        }
+
+        if (results[0]) {
+            return res.status(StatusCodes.OK).json(results[0]);
+        } else {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+            
+    })
 };
 
 const booksByCategory = (req, res) => {
