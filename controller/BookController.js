6c25f428197d9a77bdb780d292ad1d9cd0a4b70b
Galaxy-104 <js_park95@naver.com>
2024-10-08 10:20:57 +0900

    feat : Update inquiry all books and category

--- controller/BookController.js
+++ controller/BookController.js
@@ -2,17 +2,40 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
 
 const allBooks = (req, res) => {
-    // 전체 도서 리스트
-    let sql = `SELECT * FROM books`;
-    conn.query(sql, 
-        (err, results) => {
-        if (err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
-        }
+    const { categoty_id } = req.query;
 
-        return res.status(StatusCodes.OK).json(results);
-    })
+    if (categoty_id) {
+        // 카테고리별 도서 조회
+        let sql = `SELECT * FROM books WHERE category_id = ?`;
+        conn.query(sql, categoty_id, 
+            (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+            }
+    
+            if (results.length) {
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.NOT_FOUND).end();
+            }
+                
+        })
+    } else {
+        // 전체 도서 리스트
+        let sql = `SELECT * FROM books`;
+        conn.query(sql, 
+            (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+            }
+
+            return res.status(StatusCodes.OK).json(results);
+        })
+    }
+
+    
 };
 
 const bookDetail = (req, res) => {
@@ -35,12 +58,7 @@ const bookDetail = (req, res) => {
     })
 };
 
-const booksByCategory = (req, res) => {
-    res.json({ message : "카테고리별 도서 목록 조회"})
-};
-
 module.exports = {
     allBooks,
-    bookDetail,
-    booksByCategory
+    bookDetail
 }
--- routes/books.js
+++ routes/books.js
@@ -2,19 +2,15 @@ const express = require('express');
 const router = expressRouter();
 const { 
     allBooks, 
-    bookDetail, 
-    booksByCategory 
+    bookDetail
 } = require('../controller/BookController');
 
 router.use(express.json());
 
-// 전체 도서 조회
+// 도서 조회
 router.get('/', allBooks);
 
 // 개별 도서 조회
 router.get('/:id', bookDetail);
-
-// 카테고리별 도서 목록 조회
-router.get('/', booksByCategory);
 
 module.exports = router;
