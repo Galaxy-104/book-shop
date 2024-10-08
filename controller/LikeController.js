b8c522e59189708e039fae2c2dd614d17c8b88db
Galaxy-104 <js_park95@naver.com>
2024-10-08 19:15:09 +0900

    feat : Implement add like

--- controller/LikeController.js
+++ controller/LikeController.js
@@ -2,7 +2,23 @@ const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes'); 
 
 const addLike = (req, res) => {
-    res.json({ message : "좋아요 추가"})
+
+    const { id } = req.params;
+    const { user_id } = req.body;
+    
+    let sql = `INSERT INTO likes (user_id, liked_book_id) VALUES (?, ?)`;
+    const values = [ user_id, id ];
+    conn.query(sql, values,
+        (err, results) => {
+        if (err) {
+            console.log(err);
+            return res.status(StatusCodes.BAD_REQUEST).end(); // BAD REQUEST
+        }
+
+        return res.status(StatusCodes.OK).json(results);
+
+    })
+
 }
 
 const cancelLike = (req, res) => {
